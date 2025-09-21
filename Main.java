1d8419dceff08e188af825cc06d52af6733280e2
khadijadas13079-prog <khadijadas13079@gmail.com>
2025-09-21 05:51:46 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+// Main.java
+public class Main {
+    /**
+     * Main method - application entry point
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
