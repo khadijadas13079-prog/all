932db9abf32b044776c00442a69bdd569dbc9eef
khadijadas13079-prog <khadijadas13079@gmail.com>
2025-09-21 05:51:58 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// main.cpp
+#include <iostream>
+
+/**
+ * Main function - program entry point
+ * @return Exit status
+ */
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
